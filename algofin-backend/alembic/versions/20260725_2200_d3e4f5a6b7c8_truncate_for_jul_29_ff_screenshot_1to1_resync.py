56ea7ece1f56fc35317bca9e504623e077c6004b
RiyazAnsariii <mdriyazansari2005@gmail.com>
2026-07-25 15:36:19 +0530

    fix(alembic): resolve duplicate revision IDs d3e4f5a6b7c8 and e4f5a6b7c8d9 by assigning unique revision IDs 112233445566 and 223344556677

--- algofin-backend/alembic/versions/20260725_2200_d3e4f5a6b7c8_truncate_for_jul_29_ff_screenshot_1to1_resync.py
+++ algofin-backend/alembic/versions/20260725_2200_d3e4f5a6b7c8_truncate_for_jul_29_ff_screenshot_1to1_resync.py
@@ -12,7 +12,7 @@ Create Date: 2026-07-25 22:00:00.000000
 from typing import Sequence, Union
 from alembic import op
 
-revision: str = 'd3e4f5a6b7c8'
+revision: str = '112233445566'
 down_revision: Union[str, None] = 'c2d3e4f5a6b7'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
--- algofin-backend/alembic/versions/20260725_2230_e4f5a6b7c8d9_truncate_for_jul_30_ff_screenshot_1to1_resync.py
+++ algofin-backend/alembic/versions/20260725_2230_e4f5a6b7c8d9_truncate_for_jul_30_ff_screenshot_1to1_resync.py
@@ -13,8 +13,8 @@ Create Date: 2026-07-25 22:30:00.000000
 from typing import Sequence, Union
 from alembic import op
 
-revision: str = 'e4f5a6b7c8d9'
-down_revision: Union[str, None] = 'd3e4f5a6b7c8'
+revision: str = '223344556677'
+down_revision: Union[str, None] = '112233445566'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
